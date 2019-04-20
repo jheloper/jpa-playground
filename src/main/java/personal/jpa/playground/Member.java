9283f6bd4c9329369add9f4203ff05b58d53ac86
Joonghyeon Kim <jheloper9102@gmail>
2019-04-20 19:28:01 +0900

    H2 의존성 추가 및 persistence.xml 설정 추가, Member 엔티티 클래스 추가

---
+++ src/main/java/personal/jpa/playground/Member.java
@@ -0,0 +1,50 @@
+package personal.jpa.playground;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
