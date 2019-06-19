7365c25c76d5041acd72fac5dbbe5fd88d67cea1
Joonghyeon Kim <jheloper9102@gmail.com>
2019-06-19 23:41:06 +0900

    EntityManager에서 EntityTransaction을 가져오는 테스트 코드 추가

--- src/test/java/personal/jpa/playground/EntityManagerTest.java
+++ src/test/java/personal/jpa/playground/EntityManagerTest.java
@@ -2,6 +2,7 @@ package personal.jpa.playground;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -22,6 +23,27 @@ public class EntityManagerTest {
     public void getEntityManager() {
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Assert.assertNotNull(entityManager);
         Assert.assertTrue(entityManager.isOpen());
+    }
+
+
+    @Test
+    public void getTransaction() {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        Assert.assertNotNull(transaction);
+        Assert.assertFalse(transaction.isActive());
+
+        transaction.begin();
+
+        Assert.assertTrue(transaction.isActive());
+
+        transaction.rollback();
+
+        Assert.assertFalse(transaction.isActive());
     }
 }
